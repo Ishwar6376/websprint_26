f4354c98582a3414bf9d289b878015de14519283
Aryangupta <aryan072806@gmail.com>
2026-01-20 00:45:39 +0530

    config file for

---
+++ client/src/pages/features.config.js
@@ -0,0 +1,11 @@
+import { WOMEN_FEATURE } from "./features/women/config"
+import { GARBAGE_FEATURE } from "./features/garbage/config"
+import { NGO_FEATURE } from "./features/ngo/config"
+import { JOBS_FEATURE } from "./features/jobs/config"
+
+export const FEATURES = [
+  WOMEN_FEATURE,
+  GARBAGE_FEATURE,
+  NGO_FEATURE,
+  JOBS_FEATURE,
+]
