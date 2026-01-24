07d5ca018a27f5b8e25528eaf3c65c943dc08e77
Ishwar Kumawat <ishwarkumawat156@gmail.com>
2026-01-24 11:01:07 +0530

    add backend route for uploading image

---
+++ server/routes/job.route.js
@@ -0,0 +1,94 @@
+import express from "express";
+import { db, FieldValue } from "../firebaseadmin/firebaseadmin.js";
+import { checkJwt } from "../auth/authMiddleware.js";
+import ngeohash from "ngeohash";
+const router = express.Router();
+
+router.post("/", checkJwt, async (req, res) => {
+  try {
+    const userId = req.auth.payload.sub;
+    const { title, description, amount, time, location } = req.body;
+
+    if (!title || !description || !amount || !time || !location) {
+      return res.status(400).json({ message: "Missing fields" });
+    }
+    const lat = Number(location?.lat);
+    const lng = Number(location?.lng);
+
+    if (!Number.isFinite(lat) || !Number.isFinite(lng)) {
+      return res.status(400).json({ message: "Invalid location" });
+    }
+
+    const geohash = ngeohash.encode(lat, lng, 4);
+    const jobRef = await db.collection("jobs").add({
+      title,
+      description,
+      amount: Number(amount),
+      time,
+      geohash,
+      location,
+      employerId: userId,
+      status: "OPEN",
+      createdAt: new Date(),
+    });
+
+    // 🔥 create chat room
+    await db
+      .collection("jobChats")
+      .doc(jobRef.id)
+      .set({
+        participants: [userId],
+        closed: false,
+        createdAt: new Date(),
+      });
+
+    res.json({
+      success: true,
+      job: { id: jobRef.id },
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Failed to create job" });
+  }
+});
+
+
+router.get("/nearby", async (req, res) => {
+  try {
+    console.log(req)
+    const lat = Number(req.query.lat);
+    const lng = Number(req.query.lng);
+
+    console.log(lat,lng)
+    if (!Number.isFinite(lat) || !Number.isFinite(lng)) {
+      return res.status(400).json({ message: "Invalid coordinates" });
+    }
+
+    const centerHash = ngeohash.encode(lat, lng, 4);
+
+    const neighbors = ngeohash.neighbors(centerHash);
+    const hashes = [centerHash, ...neighbors];
+
+    const jobs = [];
+
+   
+    for (const hash of hashes) {
+      const snap = await db
+        .collection("jobs")
+        .where("geohash", "==", hash)
+        .where("status", "==", "OPEN")
+        .get();
+
+      snap.docs.forEach((doc) => {
+        jobs.push({ id: doc.id, ...doc.data() });
+      });
+    }
+    console.log(jobs);
+    res.json({ jobs });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Failed to fetch nearby jobs" });
+  }
+});
+
+export default router;
