ccc8bdc647bd6aea0eefdbc06d3247c5571722ca
Arushi Nayak <nayakarushi2005@gmail.com>
2026-01-27 19:09:40 +0530

    ngo controllers

--- server/src/routes/complaintHistory.routes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { getComplaintHistoryForUser } from "../controllers/complaintHistory.controller.js";
-import { checkJwt } from "../auth/authMiddleware.js";
-
-const router = express.Router();
-
-router.get("/:userId", checkJwt, getComplaintHistoryForUser);
-export default router;
--- server/src/routes/complaintStats.routes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { getComplaintStatsForUser } from "../controllers/complaintStats.controller.js";
-import { checkJwt } from "../auth/authMiddleware.js";
-
-const router = express.Router();
-
-router.get("/:userId",checkJwt, getComplaintStatsForUser);
-
-export default router;
