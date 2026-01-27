527f6cdafd3edac7ea795f50fa3693970e1dc115
Aryangupta <aryan072806@gmail.com>
2026-01-27 16:40:43 +0530

    added state for report agent

---
+++ agents/state.py
@@ -0,0 +1,56 @@
+from typing import List, Optional, TypedDict, Dict,Literal
+from pydantic import BaseModel, Field
+from enum import Enum
+class SeverityLevel(str, Enum):
+    LOW = "LOW"
+    MEDIUM = "MEDIUM"
+    HIGH = "HIGH"
+    CRITICAL = "CRITICAL"
+
+class ReportStatus(str, Enum):
+    INITIATED = "INITIATED"
+    VERIFIED = "VERIFIED"
+    ASSIGNED = "ASSIGNED"
+    RESOLVED = "RESOLVED"
+
+class ReportCategory(str, Enum):
+    WATER = "WATER"
+    INFRASTRUCTURE = "INFRASTRUCTURE"
+    WASTE = "WASTE"
+    ELECTRICITY = "ELECTRICITY"
+    UNCERTAIN = "UNCERTAIN"
+class AgentAnalysis(BaseModel):
+    confidence: float = Field(description="Confidence score 0.0 to 1.0")
+    severity: SeverityLevel
+    reasoning: str
+    title:str
+    
+class Location(BaseModel):
+    lat: float
+    lng: float
+
+class AgentState(TypedDict):
+    userId:str
+    imageUrl: str
+    location: Location
+    address: str
+    email: str
+    reportId: Optional[str]
+    description:str
+    geohash:str
+    locality_imageUrl:Optional[str]
+    locality_email:Optional[str]
+    locality_userId:Optional[str]
+    locality_reportId:Optional[str]
+    water_analysis: Optional[AgentAnalysis]
+    waste_analysis: Optional[AgentAnalysis]
+    infra_analysis: Optional[AgentAnalysis]
+    electric_analysis: Optional[AgentAnalysis]
+    uncertain_analysis:Optional[AgentAnalysis]
+    aiAnalysis: Optional[str]
+    severity: Optional[SeverityLevel]
+    assigned_category: Optional[ReportCategory]
+    status: ReportStatus
+    route:str
+    updatedRoute:str
+    tool:Literal["SAVE","UPDATE"]
