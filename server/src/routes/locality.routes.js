3746e7515f61fb7dbcf53b68c2814f2079c6812f
Aryangupta <aryan072806@gmail.com>
2026-01-30 14:22:36 +0530

    changes locality routes

--- server/src/routes/locality.routes.js
+++ server/src/routes/locality.routes.js
@@ -4,11 +4,9 @@ import {electricityCheck} from "../controllers/localityCheck/electrictiyCheck.js
 import { wasteCheck } from "../controllers/localityCheck/wasteCheck.js";
 import { waterCheck } from "../controllers/localityCheck/waterCheck.js";
 import { infraCheck } from "../controllers/localityCheck/infraCheck.js";
-import { fireCheck } from "../controllers/localityCheck/fireCheck.js";
 router.post('/waterCheck',waterCheck)
 router.post('/wasteCheck',wasteCheck)
 router.post('/infraCheck',infraCheck)
 router.post('/electricityCheck',electricityCheck)
-router.post("/fireCheck", fireCheck);
 
 export default router
